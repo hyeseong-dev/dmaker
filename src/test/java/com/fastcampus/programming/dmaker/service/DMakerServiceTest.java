a3977edbb948362523e67e7400c290f1508ea198
HYE SEONG LEE <hyeseong @dtto9.com>
2023-12-26 15:27:25 +0900

    test - DeveloerService code

---
+++ src/test/java/com/fastcampus/programming/dmaker/service/DMakerServiceTest.java
@@ -0,0 +1,54 @@
+package com.fastcampus.programming.dmaker.service;
+
+import com.fastcampus.programming.dmaker.dto.DeveloperDetailDto;
+import com.fastcampus.programming.dmaker.dto.DeveloperDto;
+import com.fastcampus.programming.dmaker.entity.Developer;
+import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
+import com.fastcampus.programming.dmaker.repository.RetiredDeveloperRepository;
+import com.fastcampus.programming.dmaker.type.DeveloperLevel;
+import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class DMakerServiceTest {
+
+    @Mock
+    private DeveloperRepository developerRepository;
+
+    @Mock
+    private RetiredDeveloperRepository retiredDeveloperRepository;
+
+    @InjectMocks
+    private DMakerService dMakerService;
+    @Test
+    public void testSomething(){
+        given(developerRepository.findByMemberId(anyString()))
+                .willReturn(Optional.of(Developer.builder()
+                                .developerLevel(DeveloperLevel.SENIOR)
+                                .developerSkillType(DeveloperSkillType.FRONT_END)
+                                .experienceYears(12)
+                                .name("name")
+                                .age(12)
+                                .build()));
+        DeveloperDetailDto developerDetailDto = dMakerService.getDeveloperDetail("memberId");
+
+        assertEquals(DeveloperLevel.SENIOR, developerDetailDto.getDeveloperLevel());
+        assertEquals(DeveloperSkillType.FRONT_END, developerDetailDto.getDeveloperSkillType());
+        assertEquals(12, developerDetailDto.getExperienceYears());
+        assertEquals("name", developerDetailDto.getName());
+        assertEquals(12, developerDetailDto.getAge());
+    }
+}
